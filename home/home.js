9690f63b2dd0b4c53d98fa275e7c67bfae97a6f6
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2021-11-15 14:28:44 +0100

    erster Funktionstest

--- home/home.js
+++ home/home.js
@@ -25,5 +25,18 @@ function site_load(){
             console.log("titel");
             header.innerHTML = "<h1>"+ data.header[0].titel +"</h1>";
         }
+        if(data.header[0].untertitel){
+            console.log("untertitel");
+            header.innerHTML = header.innerHTML + "<h2>"+ data.header[0].untertitel +"</h2>";
+        }
+    }
+    if(document.getElementById("nav")){
+        console.log("navigation");
+        var nav = document.getElementById("nav");
+        if(data.nav[0]){
+            for(i=0; i<data.nav[0].length; i++){
+                nav.innerHTML = nav.innerHTML +"<a href='"+ data.nav[i].link +"'>"+ data.nav[i].titel +"</a>";
+            }
+        }
     }
 }
